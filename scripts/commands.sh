ce75e5009c8f422053da4023f6b71ad48dcd8f3e
WelBert-dev <wellison.bertelli@hotmail.com>
2024-01-10 15:10:27 -0300

    Add configurações base para o dockerfile e scripts bash para ser executado quando montar a imagem no container

---
+++ scripts/commands.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# O shell irá encerrar a execução do script quando um comando falhar
+set -e
+
+# Fica travado aqui enquanto o postgre não iniciar
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+  sleep 2
+done
+
+echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
