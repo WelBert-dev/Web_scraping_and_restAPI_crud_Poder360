9dd5603e795e834ada8e940ee00f456d8801457e
WelBert-dev <wellison.bertelli@hotmail.com>
2024-01-11 15:20:08 -0300

    ScraperAPI e Refactoring: Add endpoint GET `http://127.0.0.1:8000/trigger_web_scraping_dou_api/?secao=`
    - Obs1: Ainda não salvando em banco, vou implementar uma flag `?saveInDB=true` depois.
    
    - Obs2: Retorno sem paginação por enquanto.
    
    Refactoring: Aplica clean code nas variáveis tornando elas mais coesas, e altera `utils.py` para `scrapers.py` centralizando todos os workers mineradores.

---
+++ djangoapp/trigger_web_scraping_dou_api/scrappers.py
@@ -0,0 +1,87 @@
+import json
+import cfscrape
+from bs4 import BeautifulSoup
+
+
+from datetime import datetime, timedelta
+import pytz
+
+
+class ScraperUtil:
+    @staticmethod
+    def run_scraper_with_section(url_param: str, secaoURLQueryString_param=None):
+        
+        # Todos argumentos presentes, Varre os DOU da seção mencionada no query string param, na data atual
+        if secaoURLQueryString_param is not None:
+
+            date_utc_now = datetime.utcnow()
+            saopaulo_tz = pytz.timezone('America/Sao_Paulo')
+            date_sp_now = date_utc_now.replace(tzinfo=pytz.utc).astimezone(saopaulo_tz)
+            date_sp_now_formated_db_pattern = date_sp_now.strftime("%d-%m-%Y")
+            
+            url_param = url_param + "?data=" + date_sp_now_formated_db_pattern + "&secao=" + secaoURLQueryString_param
+            
+            return ScraperUtil.run_generic_scraper(url_param)
+        
+        # Apenas url_param presente, Varre tudo da home do https://www.in.gov.br/leiturajornal
+        else:
+            
+            return ScraperUtil.run_generic_scraper(url_param)
+            
+    
+
+
+    @staticmethod
+    def run_generic_scraper(url_param: str):
+
+        scraper = cfscrape.create_scraper()
+        response = scraper.get(url_param)
+
+        if response.status_code == 200:
+            
+            site_html_str = BeautifulSoup(response.text, "html.parser")
+
+            all_scriptTag_that_contains_journals_json =  site_html_str.find('script', {'id': 'params'})
+
+            if all_scriptTag_that_contains_journals_json:
+
+                scriptTag_that_contains_journals_json = all_scriptTag_that_contains_journals_json.contents[0]
+
+                journals_json = json.loads(scriptTag_that_contains_journals_json)
+
+                jsonArrayField = journals_json.get("jsonArray")
+
+                if jsonArrayField:
+            
+                    return jsonArrayField
+                
+                else:
+
+                    # Nenhuma matéria postada no dia atual, pega o dia anterior.
+                    # Função recursiva, que fica fazendo rollback do dia até encontrar dados:
+                    ScraperUtil.scrape_previous_day()
+            else:
+                
+                return "Tag <script id='params'> não encontrada.\nView do DOU sofreu mudanças! ;-;"
+
+        else:
+            
+            return "Falha na requisição. Código de status: " + response.status_code
+    
+
+    @staticmethod
+    def scrape_previous_day():
+        # # Nenhuma matéria postada no dia atual, pega o dia anterior.
+        # date_utc_now = datetime.utcnow()
+        # saopaulo_tz = pytz.timezone('America/Sao_Paulo')
+        # date_sp_now = date_utc_now.replace(tzinfo=pytz.utc).astimezone(saopaulo_tz)
+        # date_sp_now_minus_one_day = date_sp_now - timedelta(days=1)
+        # date_sp_now_minus_one_day_formated_db_pattern = date_sp_now_minus_one_day.strftime("%d-%m-%Y")
+
+        # # Modifica a URL para apontar para o dia anterior
+        # new_url = f"modificar_sua_url_aqui_para_apontar_para_{date_sp_now_minus_one_day_formated_db_pattern}"
+
+        # # Chama a função run_generic_scraper novamente com a nova URL
+        # return ScraperUtil.run_generic_scraper(new_url)
+        
+        return "Nenhum jornal postado neste dia!\nPegar dias anteriores recursivamente em desenvolvimento..."
--- djangoapp/trigger_web_scraping_dou_api/views.py
+++ djangoapp/trigger_web_scraping_dou_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .utils import ScraperUtil
+from .scrappers import ScraperUtil
 
 import os
 
@@ -14,15 +14,32 @@ class ScraperViewSet(APIView):
         # Se não existem parâmetros
         if secaoURLQueryString is None and dataURLQueryString is None:
             return Response(self.handle_URL_empty_params())
-    
-        return Response({'mensagem': 'SEM RESULTADO'})
 
- 
+        # Se ?section= foi passado no URL query string param
+        elif secaoURLQueryString == "do1" or secaoURLQueryString == "do2" or secaoURLQueryString == "do3":
+            return Response(self.handle_secaoURLQueryString_param_notEmpty(secaoURLQueryString))
+
+        return Response("Nenhum jornal encontrado! ;-;")
+
+
+
+    
+    # --------------------- [ Handlers área ] ---------------------
+
     # Varre tudo da home do https://www.in.gov.br/leiturajornal
     # - GET http://127.0.0.1:8000/trigger_web_scraping_dou_api/ 
     def handle_URL_empty_params(self):
        
-        return ScraperUtil.run_scraper(DOU_BASE_URL)
+        return ScraperUtil.run_generic_scraper(DOU_BASE_URL)
+    
+
+
+    # Varre os DOU da seção mencionada no query string param, na data atual
+    # - GET http://127.0.0.1:8000/trigger_web_scraping_dou_api/?secao=`do1 | do2 | do3`
+    def handle_secaoURLQueryString_param_notEmpty(self, secaoURLQueryString_param):
+        
+        return ScraperUtil.run_scraper_with_section(DOU_BASE_URL, secaoURLQueryString_param)
+    
 
 
 
