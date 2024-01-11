a84c6a70753eb1f4642ba342a12bb5018b9e40d1
WelBert-dev <wellison.bertelli@hotmail.com>
2024-01-11 19:33:01 -0300

    ScraperAPI Refactoring no `scrappers.py`: Renomeia para `scrapers.py`

--- djangoapp/trigger_web_scraping_dou_api/views.py
+++ djangoapp/trigger_web_scraping_dou_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .scrappers import ScraperUtil
+from .scrapers import ScraperUtil
 from .validators import URLQueryStringParameterValidator
 
 import os
