40e74a8f993525ab2e6c1f046a49b98c28bca609
WelBert-dev <wellison.bertelli@hotmail.com>
2024-01-13 19:59:25 -0300

    Add comentário no service JournalJsonArrayOfDOUService de inserir os elementos do jsonArray (tag script id params):
    - ignore_conflicts=True continua a inserir os objetos em cascata e quando encontra duplicatas não insere (ignora esse elemento) e continua operação..
    
    - Removi mais verificações para ganho de performance. (Desta forma esta resolvendo bem e não está duplicando elementos iguais, mais testes vão ser implementados no tests.py depois)

--- djangoapp/trigger_web_scraping_dou_api/services.py
+++ djangoapp/trigger_web_scraping_dou_api/services.py
@@ -10,4 +10,6 @@ class JournalJsonArrayOfDOUService:
             # Normaliza a data no formato do db
             single_dou_journal_value['pubDate'] = datetime.strptime(single_dou_journal_value['pubDate'], "%d/%m/%Y").strftime("%Y-%m-%d")
 
+        # ignore_conflicts=True continua a inserir os objetos em cascata e quando encontra duplicatas não insere (ignora esse elemento) e continua operação..
+        # Removi mais verificações para ganho de performance. (Desta forma esta resolvendo bem e não está duplicado, mais testes vão ser implementados no tests.py depois)
         JournalJsonArrayOfDOU.objects.bulk_create([JournalJsonArrayOfDOU(**item) for item in dou_journals_jsonArrayField_dict], ignore_conflicts=True)
